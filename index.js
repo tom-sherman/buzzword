8e65b877a39484b102fcbbb00ad4a2ec3421a43b
Tom Sherman <the.tomsherman@gmail.com>
2019-03-05 09:01:30 +0000

    Add JSON header

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const buzzword = require('./buzzword')
 
 module.exports = (req, res) => {
+  res.setHeader('Content-Type', 'application/json')
   res.end(JSON.stringify({
     buzzword: buzzword()
   }))
